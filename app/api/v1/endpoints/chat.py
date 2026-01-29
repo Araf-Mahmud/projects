aed8f7690e24b34d1b9f33c7dee9d685a7bb5ac8
Araf Mahmud <arafmahmud@Arafs-MacBook-Air.local>
2026-01-29 17:10:07 +0600

    Context Aware

--- app/api/v1/endpoints/chat.py
+++ app/api/v1/endpoints/chat.py
@@ -1,13 +1,32 @@
 from app.schema.chat_schema import ChatRequest, ChatResponse
 from app.components.components import run_chat
+from app.components.session_manger import SessionManager
 from fastapi import APIRouter
 
 router = APIRouter()
+session_manager = SessionManager()
 
 @router.post('/chat', response_model = ChatResponse)
 async def chat_endpoint( request : ChatRequest):
     
-    chat_response = await run_chat(request.query)
+    previous_conversations = session_manager.get_session_history(request.user_id, request.session_id)
+
+    
+    chat_response = await run_chat(request.query, previous_conversations)
+
+    session_manager.store_messages(
+        user_id = request.user_id,
+        session_id = request.session_id,    
+        role = 'user',
+        content = request.query
+    )
+    
+    session_manager.store_messages(
+        user_id = request.user_id,
+        session_id = request.session_id,    
+        role = 'assistant',         
+        content = chat_response
+    )
     
     return ChatResponse(
         user_id = request.user_id,

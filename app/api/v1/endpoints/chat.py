42ce624c9aa1e50ce42387b55061588b11924a07
Araf Mahmud <arafmahmud@Arafs-MacBook-Air.local>
2026-02-01 11:26:20 +0600

    Test Response time- around 3 sec

--- app/api/v1/endpoints/chat.py
+++ app/api/v1/endpoints/chat.py
@@ -1,38 +1,41 @@
 from app.schema.chat_schema import ChatRequest, ChatResponse
 from app.components.components import run_chat
 from app.components.session_manger import SessionManager
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 
 router = APIRouter()
 session_manager = SessionManager()
 
+def store_message(user_id, session_id, query, response ):
+    
+    session_manager.store_messages(
+        user_id = user_id,
+        session_id = session_id,    
+        role = 'user',
+        content = query
+    )
+    
+    session_manager.store_messages(
+        user_id = user_id,
+        session_id = session_id,    
+        role = 'assistant',         
+        content = response
+    )
+    
 @router.post('/chat', response_model = ChatResponse)
-async def chat_endpoint( request : ChatRequest):
+async def chat_endpoint( request : ChatRequest, background_tasks: BackgroundTasks):
     
     previous_conversations = session_manager.get_session_history(request.user_id, request.session_id)
-
     
     chat_response = await run_chat(request.query, previous_conversations)
-
-    session_manager.store_messages(
-        user_id = request.user_id,
-        session_id = request.session_id,    
-        role = 'user',
-        content = request.query
-    )
     
-    session_manager.store_messages(
-        user_id = request.user_id,
-        session_id = request.session_id,    
-        role = 'assistant',         
-        content = chat_response
-    )
+    background_tasks.add_task(store_message, request.user_id, request.session_id, request.query, chat_response)
     
     return ChatResponse(
         user_id = request.user_id,
         session_id = request.session_id,
         query = request.query,
-        response = chat_response
-
+        response = chat_response,
+        background_tasks = background_tasks
     )
 

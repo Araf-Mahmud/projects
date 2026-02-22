54c8249b0c38f120ec058e0677585bc51b1a312d
Araf Mahmud <arafmahmud@Arafs-MacBook-Air.local>
2026-02-22 15:17:54 +0600

    LLM Service

--- app/components/components.py
+++ app/components/components.py
@@ -1,15 +1,19 @@
 import asyncio
-from app.services.ai_services.rag_service import CustomRAG
-from app.components.generate_response import GetAIRespnse
 
-async def run_chat( query : str, previous_conversations : str = None):
+from app.services.ai_services.rag_service import CustomRAG
+from app.services.ai_services.generate_response import GetAIRespnse
+
+
+async def run_chat( query : str, previous_conversations : str = None, trace):
     
     rag = CustomRAG()
+    
+    with trace.span(name = "retrieval"):
 
-    retrieved_context = await asyncio.to_thread(
-        rag.retrieve_documents,
-        query
-    )
+        retrieved_context = await asyncio.to_thread(
+            rag.retrieve_documents,
+            query
+        )
     
     result = GetAIRespnse(query, retrieved_context, previous_conversations).response
 
